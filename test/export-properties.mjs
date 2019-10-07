6de537ec9c25c87f3f5b41be0dd0f353a9f83e28
Lauri Eskola <lauri.eskola@acquia.com>
2019-10-07 19:44:46 +0300

    Failing test

--- test/export-properties.mjs
+++ test/export-properties.mjs
@@ -2,5 +2,7 @@ export const customProperties = {
 	'--ref-color': 'var(--color)',
 	'--color': 'rgb(255, 0, 0)',
 	'--circular': 'var(--circular-2)',
-	'--circular-2': 'var(--circular)'
+	'--circular-2': 'var(--circular)',
+	'--line-height': '1.5',
+	'--font-family': 'Arial,,,sans-serif'
 };
